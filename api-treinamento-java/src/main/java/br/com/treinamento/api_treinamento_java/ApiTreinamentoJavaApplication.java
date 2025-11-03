bed3b878fcb97202ce87f528b60d943f1fb04b8a
Paulo Roberto <devpaulorag@gmail.com>
2025-11-03 19:52:59 -0300

    feat(api): adiciona api em java com CRUD completo

---
+++ api-treinamento-java/src/main/java/br/com/treinamento/api_treinamento_java/ApiTreinamentoJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinamento.api_treinamento_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTreinamentoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTreinamentoJavaApplication.class, args);
+	}
+
+}
