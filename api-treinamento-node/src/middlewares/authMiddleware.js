842d41430d078d12acb5a259dab7b6d0a690d1db
Paulo Roberto <devpaulorag@gmail.com>
2025-11-20 19:41:19 -0300

    feat(api-node): implementa middleware de autenticação JWT e protege rotas de usuário

---
+++ api-treinamento-node/src/middlewares/authMiddleware.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({ error: "Token não fornecido" });
+    }
+
+    const parts = authHeader.split(" ");
+
+    if (parts.length !== 2) {
+        return res.status(401).json({ error: "Erro no formato do token" });
+    }
+
+    const [scheme, token] = parts;
+
+    if (!/^Bearer$/i.test(scheme)) {
+        return res.status(401).json({ error: "Token malformatado" });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoder) => {
+        if (err) {
+            return res.status(401).json({ error: "Token inválido" });
+        }
+
+        res.userId = decoder.userId;
+
+        return next();
+    });
+};
